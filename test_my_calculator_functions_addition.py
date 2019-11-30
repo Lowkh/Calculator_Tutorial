2f6f7b735029be22ddc59a2a43e1f2be35a09547
Lowkh <54532616+Lowkh@users.noreply.github.com>
2019-11-30 21:28:29 +0800

    Update test_my_calculator_functions_addition.py
    corrected assert

--- test_my_calculator_functions_addition.py
+++ test_my_calculator_functions_addition.py
@@ -9,7 +9,7 @@ def test_add(a,b,result):
 
 def test_add_negative_positive():
     value = add(-1,2,3)
-    assert value == 6
+    assert value == 4
 
 def test_add_negative_negative():
     value = add(-1,-2)
